ece65418a77a89398d0d43ac7f455b1651ebb69c
Katie <katie@quoteit.ca>
2020-11-12 03:41:34 -0500

    started custom gym env

---
+++ Python/custom_env.py
@@ -0,0 +1,53 @@
+import gym
+from gym import spaces
+
+class CustomEnv(gym.Env):
+
+    metadata = {'render.modes': []}
+
+    # reward is ((our health - enemy health) / max health)
+    reward_range = (0, 1)
+    spec = None
+
+    action_space = None
+    observation_space = None
+    
+    # not sure about the arg1 or arg2, will need to change
+    def __init__(self, arg1, arg2):
+        super(CustomEnv, self).__init__()
+
+        # action space is whether or not the button is pressed
+        # it's binary so there are 2 possible values (true or false)
+        self.action_space = spaces.Tuple((
+            spaces.Discrete(2), # UP
+            spaces.Discrete(2), # DOWN
+            spaces.Discrete(2), # LEFT
+            spaces.Discrete(2), # RIGHT
+            spaces.Discrete(2), # Y
+            spaces.Discrete(2), # X
+            spaces.Discrete(2), # L
+            spaces.Discrete(2), # B
+            spaces.Discrete(2), # A
+            spaces.Discrete(2), # R
+        ))
+
+       self.observation_space = spaces.Tuple((
+           spaces.Discrete(177), # our health [0, 176]
+           spaces.Discrete(177), # enemy health [0, 176]
+           spaces.Discrete(3), # 1 is standing, 2 is crouch, 3 is jumping
+           spaces.Discrete(2), # 0 is not attacking, 1 is attacking
+           spaces.Discrete(4), # 0 is punch, 2 is kick (from the lua) - not sure about not attacking and grab
+           spaces.Discrete(2), # whether or not there's a projectile
+           spaces.Discrete(385), # distance between two players in pixels [0, 385] (need to confirm)
+           spaces.Box(np.array([0,99])) # time remaining in the round
+        ))
+
+    def step(self, action):
+        # execute one time step within environment
+        
+        # call keras
+        # get action back
+        # send to socket server
+
+    def reset(self):
+        # reset environment state to initial state
--- startup.bat
+++ startup.bat
@@ -1,3 +1,3 @@
 start cmd.exe /k python Python/socket_server.py
-cd "emulator and rom"
+cd "emulator and rom/BizHawk-2.5.2"
 Emuhawk.exe --socket_ip=127.0.0.1 --socket_port=8080
