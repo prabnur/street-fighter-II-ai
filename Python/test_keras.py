97e280dc95eb8725258cafcbb7cf0889d30b60b9
prabnur <prabnurbal@gmail.com>
2020-11-16 14:43:22 +0530

    Used with statement to ensure that env gets closed when done

--- Python/test_keras.py
+++ Python/test_keras.py
@@ -1,7 +1,9 @@
 from rl.agents.dqn import DQNAgent
+import gym
+import gym_sf2
 
-env = gym.make('sf2-v0')
-agent = DQNAgent(env.action_space)
-agent.load_weights('weights.hdf5')
+with gym.make('sf2-v0') as env:
+  agent = DQNAgent(env.action_space)
+  agent.load_weights('weights.hdf5')
 
 # Make it play a game
--- Python/train_keras.py
+++ Python/train_keras.py
@@ -12,35 +12,35 @@ from rl.memory import SequentialMemory
 # from rl.agents import DQNAgent
 
 print("Making Gym")
-env = gym.make('sf2-v0')
-# nb_actions = env.action_space.n
-nb_actions = 10
-print("Gym initialized")
-shape = (1,)
-for key in env.observation_space:
-    shape += env.observation_space[key].shape
+with gym.make('sf2-v0') as env:
+    # nb_actions = env.action_space.n
+    nb_actions = 10
+    print("Gym initialized")
+    shape = (1,)
+    for key in env.observation_space:
+        shape += env.observation_space[key].shape
 
-print("making model")
-model = Sequential()
-model.add(Flatten(input_shape=shape))
-model.add(Dense(16))
-model.add(Activation('relu'))
-model.add(Dense(16))
-model.add(Activation('relu'))
-model.add(Dense(16))
-model.add(Activation('relu'))
-model.add(Dense(nb_actions))
-model.add(Activation('linear'))
-print(model.summary())
+    print("making model")
+    model = Sequential()
+    model.add(Flatten(input_shape=shape))
+    model.add(Dense(16))
+    model.add(Activation('relu'))
+    model.add(Dense(16))
+    model.add(Activation('relu'))
+    model.add(Dense(16))
+    model.add(Activation('relu'))
+    model.add(Dense(nb_actions))
+    model.add(Activation('linear'))
+    print(model.summary())
 
-print("makin memory and policy")
-memory = SequentialMemory(limit=50000, window_length=1)
-policy = BoltzmannQPolicy()
+    print("makin memory and policy")
+    memory = SequentialMemory(limit=50000, window_length=1)
+    policy = BoltzmannQPolicy()
 
-agent = DQNAgent(model=model, nb_actions=nb_actions, memory=memory, nb_steps_warmup=10, target_model_update=13-2, policy=policy)
-agent.compile(Adam(lr=1e-3), metrics=['mae'])
-agent.fit(env, nb_steps=10000) # This function hasa bunch of options we can play with
-# See https://keras-rl.readthedocs.io/en/latest/agents/overview/#fit for all of them
-# Also returns a keras.callbacks.History that records its training
-agent.save_weights('weights.hdf5', overwrite=True) 
-# we can save agents with different weights in different files and see how they do in arcade
+    agent = DQNAgent(model=model, nb_actions=nb_actions, memory=memory, nb_steps_warmup=10, target_model_update=13-2, policy=policy)
+    agent.compile(Adam(lr=1e-3), metrics=['mae'])
+    agent.fit(env, nb_steps=10000) # This function hasa bunch of options we can play with
+    # See https://keras-rl.readthedocs.io/en/latest/agents/overview/#fit for all of them
+    # Also returns a keras.callbacks.History that records its training
+    agent.save_weights('weights.hdf5', overwrite=True) 
+    # we can save agents with different weights in different files and see how they do in arcade
